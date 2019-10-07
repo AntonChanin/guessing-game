a7575137ca01bd856a49aae45fd649e7964d89f1
antonvh <antonhaninvl@gmail.com>
2019-10-07 18:28:33 +0300

    feat: add Binary search algorithm but not find number

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,41 @@
+var output = 0;
+var low, high = 0;
+var mid = 0;
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        low = min;
+        high = max-1;
     }
 
     guess() {
-
+        console.log(number);
+        while (low <= high) {
+            mid = (low + high) / 2;
+            output = mid;
+            if (number == mid) {
+                output = mid;
+            }
+            else {
+                if (number > mid) {
+                    this.lower();
+                } else {
+                    this.greater();
+                }
+            }
+        }
+        return parseInt(output);
     }
 
     lower() {
-
+        return low = mid + 1;
     }
 
     greater() {
-
+        return high = mid - 1;
     }
 }
 
